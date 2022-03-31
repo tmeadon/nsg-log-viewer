6fb88f3a1e8fa81b78e4061a47f2fbf73465dbad
Tom Meadon <tom@meadon.me>
2022-03-31 10:15:02 +0100

    update project name

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using web;
+using NsgLogViewer;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
