591b9b809a6a86bbbbc83de0f4d16c268c8cdd70
Tom Meadon <tom@meadon.me>
2022-04-06 21:35:11 +0100

    add loading dialog to mask single-threading and refactor

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -12,5 +12,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<DialogService>();
 
 await builder.Build().RunAsync();
